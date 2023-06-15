69b6e32e6e6055c6437b2b2cba46f46fb0a7bdc4
zhougz520 <zhougz520@gmail.com>
2023-06-15 12:55:45 +0800

    Redis 分布式锁使用 Demo

--- src/DotNetCoreStudy.Application/Authors/AuthorAppService.cs
+++ src/DotNetCoreStudy.Application/Authors/AuthorAppService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Caching;
+using Volo.Abp.DistributedLocking;
 using Volo.Abp.Domain.Repositories;
 
 namespace DotNetCoreStudy.Authors
@@ -14,15 +15,54 @@ namespace DotNetCoreStudy.Authors
         private readonly IAuthorRepository _authorRepository;
         private readonly AuthorManager _authorManager;
         private readonly IDistributedCache<AuthorDto> _cache;
+        private readonly IAbpDistributedLock _distributedLock;
 
         public AuthorAppService(
             IAuthorRepository authorRepository,
             AuthorManager authorManager,
-            IDistributedCache<AuthorDto> cache)
+            IDistributedCache<AuthorDto> cache,
+            IAbpDistributedLock distributedLock)
         {
             _authorRepository = authorRepository;
             _authorManager = authorManager;
             _cache = cache;
+            _distributedLock = distributedLock;
+        }
+
+        public async Task RedisDistributedLockTest()
+        {
+            // 定义一个List用来存放并发任务
+            List<Task> tasks = new List<Task>();
+            for (var i = 0; i < 20; i++)
+            {
+                // 将20个任务添加到List中等待并发执行
+                tasks.Add(
+                    Task.Run(async () =>
+                    {
+                        // 使用分布式锁_distributedLock.TryAcquireAsync来对金额进行增减操作
+                        await using (
+                        var handle = await _distributedLock.TryAcquireAsync(
+                            "acc_000",
+                            TimeSpan.FromSeconds(30)
+                            )
+                        )
+                        {
+                            if (handle != null)
+                            {
+                                int num = new Random().Next(1000);
+                                Console.WriteLine($"-acc_{num}账户减少{num}元");
+                                await Task.Delay(num);
+                                Console.WriteLine($"+结算账户acc_001增加{num}元");
+                            }
+                        }
+                    })
+                    );
+            }
+
+            // 等待20个任务并发全部执行完毕
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("所有任务已执行完毕！！");
         }
 
         [Authorize(DotNetCoreStudyPermissions.Authors.Create)]
--- src/DotNetCoreStudy.Application/DotNetCoreStudyApplicationModule.cs
+++ src/DotNetCoreStudy.Application/DotNetCoreStudyApplicationModule.cs
@@ -1,8 +1,13 @@
+using Medallion.Threading;
+using Medallion.Threading.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 using System;
 using Volo.Abp.Account;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.Caching;
 using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.DistributedLocking;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
@@ -11,7 +16,6 @@ using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
 
 namespace DotNetCoreStudy;
-
 [DependsOn(
     typeof(DotNetCoreStudyDomainModule),
     typeof(AbpAccountApplicationModule),
@@ -23,6 +27,7 @@ namespace DotNetCoreStudy;
     typeof(AbpSettingManagementApplicationModule)
     )]
 [DependsOn(typeof(AbpCachingStackExchangeRedisModule))]
+[DependsOn(typeof(AbpDistributedLockingModule))]
 public class DotNetCoreStudyApplicationModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
@@ -32,10 +37,27 @@ public class DotNetCoreStudyApplicationModule : AbpModule
             options.AddMaps<DotNetCoreStudyApplicationModule>();
         });
 
+        // 配置分布式缓存
         Configure<AbpDistributedCacheOptions>(options =>
         {
             options.KeyPrefix = "DotNetCoreStudy_";
             options.GlobalCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10);
+        });
+
+        // 配置分布式锁
+        Configure<AbpDistributedLockOptions>(options =>
+        {
+            options.KeyPrefix = "DotNetCoreStudy_Lock_";
+        });
+
+        // 通过单例模式注入分布式锁
+        var configuration = context.Services.GetConfiguration();
+        context.Services.AddSingleton<IDistributedLockProvider>(sp =>
+        {
+            var connection = ConnectionMultiplexer
+                .Connect(configuration["Redis:Configuration"]);
+            return new
+                RedisDistributedSynchronizationProvider(connection.GetDatabase());
         });
     }
 }
